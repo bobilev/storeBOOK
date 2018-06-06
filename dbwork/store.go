752c7ddc1c6634b7add9648a597dccec841d6642
bobilev <l7upoy@gmail.com>
2018-06-07 03:59:43 +0500

    add api addstore golang

--- dbwork/store.go
+++ dbwork/store.go
@@ -18,18 +18,18 @@ func SelectStores(author string) []dbtypes.Store{
 		var Store dbtypes.Store
 		err = res.Scan(&Store.Storeid,&Store.Name,&Store.Media,&Store.Author,&Store.Direction,&Store.Description,pq.Array(&Store.Genre),&Store.Restriction)
 		checkErr(err)
-		
+
 		Lists = append(Lists,Store)
 	}
 	return Lists
 }
-func CreateNewStore(name,direction,description,media string) int{
+func CreateNewStore(name,media,author,direction,description string) int{
 	db := dbConnect()
 	defer db.Close()
 
 	var LastInsertId int
 	// insert
-	err := db.QueryRow("INSERT INTO stores(name,direction,description,media) values($1,$2,$3,$4) returning id",name,direction,description,media).Scan(&LastInsertId)
+	err := db.QueryRow("INSERT INTO stores(name,media,author,direction,description) values($1,$2,$3,$4,$5) returning id",name,media,author,direction,description).Scan(&LastInsertId)
 	checkErr(err)
 
 	log.Println("{INSERT | Add New Store}",LastInsertId)
--- handler/api.go
+++ handler/api.go
@@ -10,7 +10,6 @@ import (
 func API(w http.ResponseWriter, r *http.Request) {
 	class := r.URL.Query().Get("class")
 	method := r.URL.Query().Get("method")
-	fmt.Println("1",class)
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Header().Set("Cache-Control", "no-cache")
@@ -28,12 +27,19 @@ func API(w http.ResponseWriter, r *http.Request) {
 		switch method {
 		case "getstoresuser":
 			user := r.URL.Query().Get("user")
-			fmt.Println("2",user)
 			res := dbwork.SelectStores(user)
 			json.NewEncoder(w).Encode(res)
 			w.WriteHeader(200)
 		case "addstore":
+			name := r.URL.Query().Get("name")
+			media := r.URL.Query().Get("media")
+			author := r.URL.Query().Get("author")
+			direction := r.URL.Query().Get("direction")
+			description := r.URL.Query().Get("description")
 
+			res := dbwork.CreateNewStore(name,media,author,direction,description)
+			json.NewEncoder(w).Encode(res)
+			w.WriteHeader(200)
 		}
 
 
